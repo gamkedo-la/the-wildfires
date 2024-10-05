df4b50a96ed58984aae7c3d4dfd368e0d31ac76e
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-10-05 06:47:52 +0100

    pressing P key again while in Pause will also Resume

--- src/scenes/pause-scene.ts
+++ src/scenes/pause-scene.ts
@@ -6,15 +6,17 @@ export class PauseScene extends Scene {
   }
 
   key_esc!: Phaser.Input.Keyboard.Key;
+  key_p!: Phaser.Input.Keyboard.Key;
 
   create() {
     this.key_esc = this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+    this.key_p = this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.P);
 
     const { width, height } = this.scale;
 
     this.add.rectangle(0, 0, width, height, 0x000000, 0.5).setOrigin(0);
 
-    const pausedText = this.add.text(width / 2, height / 2 - 50, 'Paused', {
+    this.add.text(width / 2, height / 2 - 50, 'Paused', {
       fontSize: '64px',
       color: '#ffffff'
     }).setOrigin(0.5);
@@ -31,7 +33,7 @@ export class PauseScene extends Scene {
   }
 
   update(time: number, delta: number) {
-    if (this.key_esc.isDown) {
+    if (this.key_esc.isDown || Phaser.Input.Keyboard.JustDown(this.key_p)) {
       this.resumeGame();
     }
   }
